26cc2d99810a573d2782f6e098ba921fadb48608
Yuri Programa <yuryjessenia@gmail.com>
2026-06-08 15:28:42 -0500

    Creación
    SE crea el proyecto y iniciamos creado la estructura de las paginas principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { 
+  IonApp, 
+  IonIcon, 
+  IonLabel, 
+  IonRouterOutlet, 
+  IonTabBar, 
+  IonTabButton, 
+  IonTabs, 
+  setupIonicReact 
+} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+
+// 1. Importamos los íconos oficiales de Ionicons
+import { homeOutline, schoolOutline, barbellOutline, personOutline } from 'ionicons/icons';
+
+// 2. Importamos tus páginas (las que vi en tu captura)
 import Home from './pages/Home';
+import Aprender from './pages/Aprender';
+import Practicar from './pages/Practicar';
+import Perfil from './pages/Perfil';
+import Login from './pages/Login';
 
-/* Core CSS required for Ionic components to work properly */
+/* Estilos obligatorios de Ionic */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
 import './theme/variables.css';
 
 setupIonicReact();
@@ -38,14 +37,53 @@ setupIonicReact();
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
+      <IonTabs>
+        
+        {/* EL CONTENEDOR DE PÁGINAS: Aquí se carga el componente según la URL */}
+        <IonRouterOutlet>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/aprender" component={Aprender} />
+          <Route exact path="/practicar" component={Practicar} />
+          <Route exact path="/perfil" component={Perfil} />
+          <Route exact path="/login" component={Login} />
+          
+          {/* Si el usuario entra a la raíz '/', lo redirigimos automáticamente a /home */}
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+
+        {/* LA BARRA DE PESTAÑAS (Física, en la parte inferior) */}
+        {/* Le ponemos slot="bottom" para que se fije abajo */}
+        <IonTabBar slot="bottom" className="custom-tab-bar">
+          
+          {/* Pestaña 1: Inicio */}
+          <IonTabButton tab="home" href="/home">
+            <IonIcon aria-hidden="true" icon={homeOutline} />
+            <IonLabel>Inicio</IonLabel>
+          </IonTabButton>
+
+          {/* Pestaña 2: Aprender */}
+          <IonTabButton tab="aprender" href="/aprender">
+            <IonIcon aria-hidden="true" icon={schoolOutline} />
+            <IonLabel>Aprender</IonLabel>
+          </IonTabButton>
+
+          {/* Pestaña 3: Practicar */}
+          <IonTabButton tab="practicar" href="/practicar">
+            <IonIcon aria-hidden="true" icon={barbellOutline} />
+            <IonLabel>Practicar</IonLabel>
+          </IonTabButton>
+
+          {/* Pestaña 4: Perfil */}
+          <IonTabButton tab="perfil" href="/perfil">
+            <IonIcon aria-hidden="true" icon={personOutline} />
+            <IonLabel>Perfil</IonLabel>
+          </IonTabButton>
+
+        </IonTabBar>
+
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
