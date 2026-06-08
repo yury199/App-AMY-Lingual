d3c69251508bd3ca3e86db9dbf0ac10f58cbcc0a
Yuri Programa <yuryjessenia@gmail.com>
2026-06-08 14:14:31 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-AMY-Lingual',
+  webDir: 'dist'
+};
+
+export default config;
